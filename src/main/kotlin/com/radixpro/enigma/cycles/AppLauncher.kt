7b7e6773fe4a6e9fa1059640acceb86ef25a933e
jan <tech@kampherbeek.com>
2021-04-02 21:10:14 +0200

    Initial empty version.

---
+++ src/main/kotlin/com/radixpro/enigma/cycles/AppLauncher.kt
@@ -0,0 +1,13 @@
+/*
+ * Jan Kampherbeek, (c)  2021.
+ * Enigma Charts is open source.
+ * Please check the file copyright.txt in the root of the source for further details
+ */
+package com.radixpro.enigma.charts
+
+object AppLauncher {
+    @JvmStatic
+    fun main(args: Array<String?>) {
+        StartEnigma().run(args)
+    }
+}
---
+++ src/main/kotlin/com/radixpro/enigma/cycles/StartEnigma.kt
@@ -0,0 +1,27 @@
+/*
+ * Jan Kampherbeek, (c)  2021.
+ * Enigma Charts is open source.
+ * Please check the file copyright.txt in the root of the source for further details
+ */
+package com.radixpro.enigma.charts
+
+import javafx.application.Application
+import javafx.stage.Stage
+
+
+/**
+ * Starts the application.
+ */
+class StartEnigma: Application() {
+
+    fun run(args: Array<String?>) {
+        launch(*args)
+    }
+
+    override fun start(primaryStage: Stage) {
+//        Injector.injectScreenStart().show()
+        println("Started...")
+
+    }
+
+}
