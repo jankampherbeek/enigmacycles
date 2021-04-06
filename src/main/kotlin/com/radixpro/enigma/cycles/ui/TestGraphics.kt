a257aa2cba80d844b0d6300e220a025c5c656eb0
jan <tech@kampherbeek.com>
2021-04-06 11:33:31 +0200

    Added POC for charts

---
+++ src/main/kotlin/com/radixpro/enigma/cycles/ui/TestGraphics.kt
@@ -0,0 +1,62 @@
+/*
+ * Jan Kampherbeek, (c)  2021.
+ * Enigma Cycles is open source.
+ * Please check the file copyright.txt in the root of the source for further details
+ */
+
+package com.radixpro.enigma.cycles.ui
+
+import com.radixpro.enigma.cycles.ui.UiDictionary.STYLESHEET
+import javafx.scene.chart.XYChart
+import javafx.scene.chart.LineChart
+import javafx.scene.Scene
+import javafx.scene.chart.NumberAxis
+import javafx.scene.layout.HBox
+import javafx.stage.Stage
+
+class TestGraphics {
+
+    fun testIt() {
+        val stage = Stage()
+        initUI(stage)
+    }
+
+    private fun initUI(stage: Stage) {
+        val root = HBox()
+        val scene = Scene(root, 450.0, 330.0)
+        val xAxis = NumberAxis()
+        xAxis.setLabel("Age")
+        val yAxis = NumberAxis()
+        yAxis.setLabel("Salary (€)")
+        val lineChart = LineChart<Number, Number>(xAxis, yAxis)
+        lineChart.title = "Average salary per age"
+        lineChart.createSymbols = false
+        val data = XYChart.Series<Number, Number>()
+        data.name = "2016"
+        data.data.add(XYChart.Data(18, 567))
+        data.data.add(XYChart.Data(20, 612))
+        data.data.add(XYChart.Data(25, 800))
+        data.data.add(XYChart.Data(30, 980))
+        data.data.add(XYChart.Data(40, 1410))
+        data.data.add(XYChart.Data(50, 2350))
+        lineChart.data.add(data)
+
+        val data2 = XYChart.Series<Number, Number>()
+        data2.name = "2017"
+        data2.data.add(XYChart.Data(18, 367))
+        data2.data.add(XYChart.Data(20, 212))
+        data2.data.add(XYChart.Data(25, 900))
+        data2.data.add(XYChart.Data(30, 780))
+        data2.data.add(XYChart.Data(40, 410))
+        data2.data.add(XYChart.Data(50, 350))
+        lineChart.data.add(data2)
+
+
+        root.children.add(lineChart)
+        stage.setTitle("LineChart")
+        stage.setScene(scene)
+        stage.show()
+    }
+
+
+}
